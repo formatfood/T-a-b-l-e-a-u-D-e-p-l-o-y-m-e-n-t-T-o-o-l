cfada507019053822339cc7a80d3e6bf6af646fd
formatfood <36201931+formatfood@users.noreply.github.com>
2018-05-16 05:40:57 +0530

    just commited

---
+++ ConsoleApp1/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Player
+    {
+        public char Id { get; set; }
+        public string PlaeryName { get; set; }
+        public string Team { get; set; }
+        public string Role { get; set; }
+        public double Points { get; set; }
+        public double Credits { get; set; }
+
+    }
+}
---
+++ ConsoleApp1/Team.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Team
+    {
+        public double TotalCredits { get; set; }
+        public int TeamACount { get; set; }
+        public int TeamBCount { get; set; }
+        public IList<Player> TeamPlayers { get; set; }
+    }
+}
